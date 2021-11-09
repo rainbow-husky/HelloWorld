602f65d6a8a7f696d57b25a15f3309d893adc7fb
1254003114@qq.com <1254003114@qq.com>
2021-11-09 15:42:18 +0800

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+// using MathOperations;
+
+namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int sum = MathOperations.Add(num1, num2);
+            Console.WriteLine($"{num1} + {num2} = {sum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathOperations/MathOperations.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Calculator
+{
+    public static class MathOperations
+    {
+        public static int Add(int num1, int num2) => num1 + num2;
+    }
+}
